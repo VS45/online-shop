8d27bd6d7765b49e95341bc3ffcc626f053f1e85
vs45 <bringfirehjake88@gmail.com>
2024-06-10 16:31:51 +0100

    CRUD

--- model/user.js
+++ model/user.js
@@ -2,6 +2,18 @@ const db = require('../data/config');
 const User = {
   create: (user,callback) => {
     return db.query('INSERT INTO users SET ?', user,callback);
+  },
+  getAll: (callback) => {
+    return db.query('SELECT * FROM users',callback);
+  },
+  getById: (id,callback) => {
+    return db.query('SELECT * FROM users WHERE id = ?', [id],callback);
+  },
+  update: (id, user,callback) => {
+    return db.query('UPDATE users SET ? WHERE id = ?', [user, id],callback);
+  },
+  delete: (id,callback) => {
+    return db.query('DELETE FROM users WHERE id = ?', [id],callback);
   }
 };
 
